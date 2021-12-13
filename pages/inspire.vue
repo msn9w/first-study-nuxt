9cad734cb97f9caf4b66ae8c1a6d5013e73cd0fd
msn9w <arikui0520@gmail.com>
2021-12-13 17:17:06 +0900

    完成(仮)

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,15 +1,17 @@
 <template>
-  <v-row>
-    <v-col>
-      
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-col>
-  </v-row>
+  <div class="d-flex flex-column justify-space-between align-center">
+    <v-row>
+      <v-col>
+        <v-img height="400" width="400" src="./mossan.jpg"></v-img>
+        <blockquote class="blockquote">
+          &#8220;First, solve the problem. Then, write the code.&#8221;
+          <footer>
+            <small>
+              <em>&mdash;John Johnson</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-col>
+    </v-row>
+  </div>
 </template>
